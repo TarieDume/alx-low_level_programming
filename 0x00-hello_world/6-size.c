68a8ef37f86d0ff35970a71680b391f29b6f6e68
TarieDume <tarienyamunda@gmail.com>
2023-02-16 14:20:39 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ *main - this program prints various types on the computer and runs on
  *
- *Return 0 (Success)
+ *Return: 0 (Success)
  */
 
 int main(void)
