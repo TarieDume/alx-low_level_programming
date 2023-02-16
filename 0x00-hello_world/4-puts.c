4b0e9650ad5ad53a9393ba24d995bf7167c7e9f6
TarieDume <tarienyamunda@gmail.com>
2023-02-16 10:26:50 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,9 @@
  * main - this program outputsa string using puts method
  * Return: 0 (Success)
  */
-int main void
+
+int main (void)
 {
-	puts ("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
