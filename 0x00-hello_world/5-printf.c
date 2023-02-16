798da4746c3f96edef2a0d22d75d2beba5e3ebcd
TarieDume <tarienyamunda@gmail.com>
2023-02-16 10:49:46 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints astring using the printf function
+ * Return: 0 (Sucecess)
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece art,\n");
+return (0);
