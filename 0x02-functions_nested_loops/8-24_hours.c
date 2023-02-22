67a4378ce303daafef1225965bd6ad98e8c6009f
TarieDume <tarienyamunda@gmail.com>
2023-02-22 05:25:45 -0800

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,14 +13,14 @@ int hour, minute;
 for (hour = 0; hour <= 23; hour++)
 {
 	for (minute = 0; minute <= 59; minute++)
-			{
-				_putchar('0' + hour / 10);
-				_putchar('0' + hour % 10);
-				_putchar(':');
-				_putchar('0' + minute / 10);
-				_putchar('0' + minute % 10);
-				_putchar('\n');
-			}
+	{
+		_putchar('0' + hour / 10);
+		_putchar('0' + hour % 10);
+		_putchar(':');
+		_putchar('0' + minute / 10);
+		_putchar('0' + minute % 10);
+		_putchar('\n');
+	}
 }
 
 }
