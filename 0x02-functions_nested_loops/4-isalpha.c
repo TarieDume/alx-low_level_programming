4e80e4a29356eabdaf379749114d91d5b1c457ad
TarieDume <tarienyamunda@gmail.com>
2023-02-22 01:13:31 -0800

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,18 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c : charactor to check
+ *
  * Return : 1 if c is a letter or 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
