554814d8c1ef85eccc9efaff92cfc95a3814e10c
TarieDume <tarienyamunda@gmail.com>
2023-02-22 04:08:47 -0800

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,42 +8,19 @@
 void jack_bauer(void)
 {
 
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
+int hour, minute;
 
-h2 = 0;
-while (h2 <= 2)
+for (hour = 0; hour <= 23; hour++)
 {
-	if (h2 == 2)
-	{
-		/*Restrain to 23h, not 29*/
-		a = 3;
-	}
-	h2 = 0;
-	while (h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
-		{
-			m1 = 0;
-			while (m1 <= 9)
+	for (minute = 0; minute <= 59; minute++)
 			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
+				_putchar('0' + hour / 10);
+				_putchar('0' + hour % 10);
 				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
+				_putchar('0' + minute / 10);
+				_putchar('0' + minute % 10);
 				_putchar('\n');
-				m1++;
 			}
-			m2++;
-		}
-		h1++;
-	}
-	h2++;
 }
 
 }
