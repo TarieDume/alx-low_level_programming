d09179cf63ebffc5aa43f232fa73ba5443fc0a22
TarieDume <tarienyamunda@gmail.com>
2023-02-23 10:29:32 -0800

    More Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *  print_most_numbers - prints the numbers, from 0 to 9,
@@ -13,8 +14,8 @@ void print_most_numbers(void)
 	while (start <= end)
 	{
 		if (start != 2 && start != 4)
-			_putchar(start + '0');
+			putchar (start + '0');
 		start++;
 	}
-	_putchar('\n');
+	putchar ('\n');
 }
