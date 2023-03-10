df9460904b6df5441e4cb625900713f0a6f5ba14
TarieDume <tarienyamunda@gmail.com>
2023-03-10 12:53:10 -0800

    static library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c : charactor to check
+ *
+ * Return: 1 on success
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
