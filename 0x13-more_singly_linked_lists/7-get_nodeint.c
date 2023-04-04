178f8cfea070152567c3a2254a0ae85f3cd8f1cf
TarieDume <tarienyamunda@gmail.com>
2023-04-04 14:42:32 -0700

    nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint -  function that returns the nth node of a linked list.
+ * get_nodeint_at_index -  function that returns the nth node of a linked list.
  * @head: head of a list.
  * @index: Index of the node.
  * Return: NULL if the node does not exist.
