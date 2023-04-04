b7187189db91a6d54c921f1d2a27f69be74cf6f7
TarieDume <tarienyamunda@gmail.com>
2023-04-04 14:12:49 -0700

    nodeint

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *get_nodeint - function that returns the nth node of a linked list.
+ *pop_listint - function that returns the nth node of a linked list.
  *@head: head of list.
  *Return: node data
  */
@@ -10,14 +10,14 @@ int pop_listint(listint_t **head)
 {
 	int node;
 	listint_t *temp;
-	
+
 	if (!head || !*head)
 		return (0);
 
 	node = (*head)->n;
 	temp = (*head)->next;
 
-	free (*head);
+	free(*head);
 	*head = temp;
 
 	return (node);
