7504904c5f42be52f1dae4d6f93b98d13e6f25ea
TarieDume <tarienyamunda@gmail.com>
2023-04-10 14:01:12 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,10 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	nrd = read(fl, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-
 	if (!filename)
 		return (0);
 
@@ -27,10 +23,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fl == -1)
 		return (0);
 
-
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
+
+	nrd = read(fl, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fl);
 
